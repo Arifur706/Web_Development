a23910acd76ea8134185377b2a902681b71c30ce
Arifur706 <81330591+Arifur706@users.noreply.github.com>
2024-02-04 03:04:23 +0600

    Delete Learn Javascript/index.js

--- Learn Javascript/index.js
+++
@@ -1,8 +0,0 @@
-function myfun(){
-    var Name1 = document.getElementById(f1)
-    var Name2 = document.getElementById(f2)
-
-    var fullname = Name1 + Name2;
-    document.getElementById(p1).innerHTML = fullname;
-
-}
