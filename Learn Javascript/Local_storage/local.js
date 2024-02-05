e881fabb48f80bd4a63c2233cb61ef090609c1f1
rejuan <arifur15-13810diu.edu.bd>
2024-02-06 00:49:41 +0600

    v3

---
+++ Learn Javascript/Local_storage/local.js
@@ -0,0 +1,12 @@
+
+//localStorage.setItem('userName', 'Arifur Rahman');
+//localStorage.setItem('Password', '123456');
+
+const userName = localStorage.getItem('userName');
+const Password  = localStorage.getItem('Password')
+
+console.log(userName, Password);
+
+localStorage.clear();
+
+
