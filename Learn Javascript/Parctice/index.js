8c9da3867d1c093f6beb7cfaffe171d21e644e72
Arifur706 <81330591+Arifur706@users.noreply.github.com>
2024-02-05 14:41:39 +0600

    Create index.js

---
+++ Learn Javascript/Parctice/index.js
@@ -0,0 +1,8 @@
+function myfun(){
+    var Name1 = document.getElementById(f1)
+    var Name2 = document.getElementById(f2)
+
+    var fullname = Name1 + Name2;
+    document.getElementById(p1).innerHTML = fullname;
+
+}
